4c3daeb2d9b42d3c29197c1c534bb99296d4d83c
Adrien M <hadolf@gmail.com>
2022-12-09 17:35:02 +0100

    remove unconnected slaves

--- hosts.js
+++ hosts.js
@@ -3,7 +3,17 @@ let hosts = {
   update: (monitor) => {
     let hostname = monitor.os.hostname
     hosts.list[hostname] = monitor
+    hosts.list[hostname].lastUpdate = new Date().getTime()
+  },
+  clear: () => {
+    for (const hostname in hosts.list)
+      if (new Date().getTime() - hosts.list[hostname].lastUpdate > 15000)
+        delete hosts.list[hostname]
   }
 }
+
+setInterval(() => {
+  hosts.clear()
+}, 15000)
 
 export default hosts
