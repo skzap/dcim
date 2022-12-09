41a8bd87e911e9a9cd947ab715830fa93a50585b
Adrien M <hadolf@gmail.com>
2022-12-09 16:50:37 +0100

    track gpu models

--- monitor.js
+++ monitor.js
@@ -1,8 +1,25 @@
 import os from 'node:os'
+import {exec} from 'child_process';
 
 let monitor = {
-  os: {}
+  os: {},
+  gpu: {}
 }
+
+exec('lshw -c video', (error, stdout, stderr) => {
+  let lines = stdout.split('\n')
+  let product, vendor
+  for (let i = 0; i < lines.length; i++) {
+    if (lines[i].indexOf('product: ') > -1) {
+      product = lines[i].slice(lines[i].indexOf('product: ')+9)
+    }
+    if (lines[i].indexOf('vendor: ') > -1) {
+      vendor = lines[i].slice(lines[i].indexOf('vendor: ')+8)
+    }
+  }
+  monitor.gpu.product = product
+  monitor.gpu.vendor = vendor
+});
 
 for (const fname in os) {
   if (typeof os[fname] === 'function') {
