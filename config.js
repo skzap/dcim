eb4a46d182351f9cf72140affe150654944969c1
Adrien M <hadolf@gmail.com>
2023-01-04 16:33:13 +0100

    add form

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let config = {
-  version: '0.1.0',
+  version: '0.1.1',
   rootDirectory: '/root/dcim/',
   master: {
     ip: '192.168.1.193',
@@ -21,27 +21,9 @@ let config = {
   }
 }
 
-// let config = {
-//   version: '0.1.0',
-//   rootDirectory: '/home/dr/Coding/dcim/',
-//   master: {
-//     ip: '127.0.0.1',
-//     ws: {
-//       port: 4545,
-//       pingInterval: 10000
-//     },
-//     http: {
-//       port: 4546,
-//       siteName: "Eiffel Cloud"
-//     }
-//   },
-//   slave: {
-//     ws: {
-//       pingInterval: 11000,
-//       monitorInterval: 5000,
-//       reconnectInterval: 5000
-//     }
-//   }
-// }
+// UNCOMMENT FOR PRODUCTION
+config.rootDirectory = '/home/dr/Coding/dcim/'
+config.master.ip = '127.0.0.1'
+// END UNCOMMENT
 
 export default config
