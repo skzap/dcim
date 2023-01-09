54f07ca8a565124abb74898b6d7007be23fc9a94
Adrien M <hadolf@gmail.com>
2023-01-09 21:41:29 +0100

    add trex miner

---
+++ assets/scripts/trex_install.sh
@@ -0,0 +1,7 @@
+wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz
+mkdir trex
+tar -xvzf t-rex-0.26.8-linux.tar.gz --directory trex
+cd trex
+cp /root/dcim/assets/services/xmrig.service /etc/systemd/system/trex.service
+systemctl enable trex
+systemctl start trex
