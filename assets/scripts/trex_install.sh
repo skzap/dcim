b35bfc75910f9f53f6cdd0c33c8c8ed705b21264
Adrien M <hadolf@gmail.com>
2023-01-20 16:23:59 +0100

    fix mistake trex

--- assets/scripts/trex_install.sh
+++ assets/scripts/trex_install.sh
@@ -1,7 +1,7 @@
+cd /root
 wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz
 mkdir trex
 tar -xvzf t-rex-0.26.8-linux.tar.gz --directory trex
-cd trex
 cp /root/dcim/assets/services/trex.service /etc/systemd/system/trex.service
 systemctl enable trex
 systemctl start trex
